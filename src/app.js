8d76249c895982ae3ad4a29384a38d21fd15800c
bilgicfidan <bilgicfidan@gmail.com>
2021-10-19 00:21:24 +0300

    first lesson

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("hi")
+
+let dollarToday=9.30
+
+let dollarYesterday=9.20
+
+{
+    let dollarYesterday=9.10
+}
+
+console.log(dollarYesterday)
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    const element = konutKredileri[index];
+    
+    console.log(element)
+}
+//console.log(konutKredileri)
